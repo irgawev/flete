16f850920b109c0be750f1695e82469dd05c1cbf
irgawev <irgawevrasul@gmail.com>
2024-05-30 14:29:29 +0500

    FT-3 | done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['example.com'],
+        minimumCacheTTL: 60
+    },
+    experimental: {
+        swcMinify: true,
+        optimizeCss: true,
+        serverSourceMaps: false
+    },
+    productionBrowserSourceMaps: false
+};
 
 export default nextConfig;
